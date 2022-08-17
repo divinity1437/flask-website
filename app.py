b6bf32dd6fceeaea5c69c3087539782f63a64154
Divinity1437 <creonplays@gmail.com>
2022-08-17 22:21:43 +0000

    a.

--- app.py
+++ app.py
@@ -6,17 +6,20 @@ import requests
 import json
 
 from flask import *
-from flask_hcaptcha import hCaptcha
+from flask_recaptcha import ReCaptcha
+from datetime import datetime, date
 from typing import Dict
 from discord import Webhook, RequestsWebhookAdapter
 from markupsafe import Markup
+from werkzeug.utils import secure_filename
+from circleguard import *
 
 webhook = Webhook.partial(998476036620685362, 'kO8LXD36-ATSJWt9cU5V6XQbF0Irqci18mpsvD9jZW56bre0Vo4LXsbdk4RHeh8heABb', adapter=RequestsWebhookAdapter())
 app = Flask(__name__)
-hcaptcha = hCaptcha(app)
-HCAPTCHA_ENABLED = True
-HCAPTCHA_SITE_KEY = "930d77f0-2a8d-4878-9390-cb0fbbc4f9fb"
-HCAPTCHA_SECRET_KEY = "0x57cc8344CEE0248B2EeeB1bDa25DE279883d80f6"
+app.config['RECAPTCHA_SITE_KEY'] = '6LeF3k8hAAAAAKdWhx5LfcJF8oSVDKSbMcXCQ8P5'
+app.config['RECAPTCHA_SECRET_KEY'] = '6LeF3k8hAAAAAL9y84Kptv26p8aj91g0GDVvEqzh'
+recaptcha = ReCaptcha(app) 
+cg = Circleguard("bf33f740eb88879490c804fc9c01884cb1b5bfa9")
 
 common = {
     'first_name': 'OwOuser',
@@ -36,6 +39,7 @@ def sharex():
     shibe = requests.get("http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true").text
     shibe_parsed = json.loads(shibe)
     shibe_aa = shibe_parsed[0]
+    
     # Total count of images in dir
     dir_path = r'/home/owo/sharex/images/'
     filecount = 0
@@ -54,7 +58,7 @@ def mail():
     if request.method == 'POST':
         email = request.form.get('email')
         discord = request.form.get('discord')
-        if re.fullmatch(regex, email) and re.fullmatch(regex2, discord) and hcaptcha.verify():
+        if re.fullmatch(regex, email) and re.fullmatch(regex2, discord) and recaptcha.verify():
             message = "Your request was successfully send. For fast response, write me in discord: OwOuser#9860"
             webhook.send(discord + ' requested for ' + email, username='dr.dre')
         else:
@@ -62,6 +66,27 @@ def mail():
 
     return render_template('mail.html', message=message, common=common)
 
+@app.route('/circleguard')
+def circleguard():
+   return render_template('circleguard.html')
+    
+@app.route('/circleguard', methods = ['GET', 'POST'])
+def circleguard_upload():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        f.filename_underscope = f.filename.replace(" ", "_").replace(']','').replace('[','').replace('(','').replace(')','').replace("'","").replace('!','').replace('~','').replace('&','').replace(',','')
+        replay = ReplayPath(r"/home/owo/owo/" + f.filename_underscope)
+        cg.load(replay)
+        print(replay)
+        # Getting replay ur, frametime, frametimes and snaps
+        replay_ur, replay_frametime, replay_frametimes, replay_snaps = cg.ur(replay), cg.frametime(replay), cg.frametimes(replay), cg.snaps(replay, max_angle=12, min_distance=6)
+        # Getting replay info such as bmap, timestamp, username, mods, 300/100/50/miss and score
+        r_timestamp, r_beatmap_id, r_username, r_mods, r_count_300, r_count_100, r_count_50, r_count_miss, r_score, r_max_combo = replay.timestamp, replay.beatmap_id, replay.username, replay.mods,replay.count_300, replay.count_100, replay.count_50, replay.count_miss, replay.score, replay.max_combo
+        print("replay was loaded and data was displayed, delete file now")
+        os.remove(f.filename_underscope)
+
+        return 'ur = {} <br/>frametime = {} <br/>replay_frametimes =  {}<br/>replay_snaps = {}<br/>Play Date = {}<br/>Beatmap_id = {}<br/>Username = {}<br/>Mods = {}<br/>300: {}<br/>100: {}<br/>50: {}<br/>Miss: {}<br/>Score = {}<br/>Max Combo: {}'.format(replay_ur, replay_frametime, replay_frametimes, replay_snaps, r_timestamp, r_beatmap_id, r_username, r_mods, r_count_300, r_count_100, r_count_50, r_count_miss, r_score, r_max_combo)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -79,4 +104,4 @@ def get_static_json(path):
 
 if __name__ == "__main__":
     print("running py app")
-    app.run(host="127.0.0.1", debug=True)
+    app.run(host="127.0.0.1", debug=False)
